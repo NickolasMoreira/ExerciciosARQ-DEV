0747328536a58008e834a43a52a2cdc6420b423c
NickolasMoreira <nickolas.tato@gmail.com>
2020-04-24 16:56:55 -0300

    Exercicio 8.1

---
+++ Exercicio8/src/Exercicio8/Noticiario.java
@@ -0,0 +1,12 @@
+package Exercicio8;
+
+/**
+ * 
+ * @author Allan Samey Cordeiro Ramos - RA:201516605 - Turma:SIN3AN-MCA
+ * 			
+ *
+ */
+
+public abstract class Noticiario {
+	public abstract void notificaNoticia(String textoNoticia, int dia, int mes, String topico);
+}
